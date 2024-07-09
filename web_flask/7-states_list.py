f79c07c97880103dfbf1b1b1819f08bdbbfbba98
BPChance <bradenpchance6@gmail.com>
2024-07-09 15:15:42 -0500

    set up flask app to display state lists in html

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" starts a Flask web app """
+
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+app = Flask(__name__)
+
+@app.route('/states_list', strict_slashes=False)
+def state_list():
+    """ display html page with list of state objects """
+    states = storage.all(State).values()
+    sorted_states = sorted(states, key=lambda state: state.name)
+    return render_template('7-states_list.html', states=sorted_states)
+
+@app.teardown_appcontext
+def tear_db(exception):
+    """ closes storage """
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
